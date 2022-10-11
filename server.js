b7e231d2a196a3071efcd3ce4cacb73ff921dfce
Alvin <alvin.li@wink.org>
2022-10-11 17:26:33 +0800

    perfect example

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ const fs = require('fs'); // 檔案存取
 const app = new Koa();
 
 app.use(mongo({
-    uri: 'mongodb+srv://alvinknight:vbjPeWtkyBvavH4G@cluster0.qlulfz3.mongodb.net/test',
+    uri: 'mongodb+srv://' +
+        'alvinknight:vbjPeWtkyBvavH4G@cluster0.qlulfz3.mongodb.net/test',
 }));
 
 app.use(KoaBody());
 
 const router = new Router();
-let lastId = 0;
-let articles = [];
 
 router
     .post('/article', ctx => {
@@ -38,24 +37,30 @@ router
             ctx.status = 400;
         }
     })
-    .put('/article/:id', ctx => {
+    .put('/article/:id', async ctx => {
         // 把資料分別存在 id、title、body、author 等變數
-        const id = parseInt(ctx.params.id);
-        const { title } = ctx.request.body;
-        const { body } = ctx.request.body;
-        const { author } = ctx.request.body;
+        const id = ctx.params.id;
+        const { title, body, author } = ctx.request.body;
 
         if (title && body && author) {
             // 如果必填資料都有，就編輯文章
             // 首先找出文章
-            const article = articles.find(x => x.id === id);
+            const article = await ctx.db.collection('articles').findOne({ _id: mongo.ObjectId(id) });
 
             if (article) {
                 // 如果有文章的話就編輯，並依照文件回傳 204
-                article.title = title;
-                article.body = body;
-                article.author = author;
-                article.time = new Date();
+                ctx.db.collection('articles')
+                    .update(
+                        { _id: mongo.ObjectId(id) },
+                        {
+                            $set: {
+                                title,
+                                body,
+                                author,
+                                time: new Date(),
+                            }
+                        });
+
                 ctx.status = 204;
             } else {
                 // 沒有找到的話就依照文件回傳 404
@@ -66,8 +71,8 @@ router
             ctx.status = 400;
         }
     })
-    .get('/article', ctx => {
-        const article = ctx.mongo.db('mongodbVSCodePlaygroundDB').collection('articles').find();
+    .get('/article', async ctx => {
+        const article = await ctx.db.collection('articles').find().toArray();
         if (article) {
             ctx.body = article;
         } else {
@@ -80,7 +85,7 @@ router
 
         if (id) {
             // 首先找出文章
-            const article = await ctx.mongo.db('mongodbVSCodePlaygroundDB').collection('articles').findOne({ _id: mongo.ObjectId(id) });
+            const article = await ctx.db.collection('articles').findOne({ _id: mongo.ObjectId(id) });
 
             if (article) {
                 // 如果有文章的話就依照文件回傳文章內容（預設就是狀態 200）
@@ -96,11 +101,11 @@ router
     })
     .delete('/article/:id', ctx => {
         // 把資料分別存在 id 變數
-        const id = parseInt(ctx.params.id);
+        const id = ctx.params.id;
 
         if (id) {
             // 首先找出文章
-            const article = articles.find(x => x.id === id);
+            ctx.db.collection('articles').remove({ _id: mongo.ObjectId(id) });
 
             if (article) {
                 // 如果有文章的話就刪除文章，然後依照文件回傳 204
